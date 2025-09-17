ac6cd534cfd90396deff276e2dd972e5041946cd
Carl Schéle <carl.schele@asurgent.se>
2025-09-17 10:43:37 +0200

    adding logging

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
+using Serilog;
+
 namespace nackademin24_episerver
 {
     public class Program
     {
-        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+        public static IConfiguration Configuration { get; } =
+        new ConfigurationBuilder()
+        .AddJsonFile("appSettings.json", false, true)
+        .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", true, true)
+        .AddJsonFile($"appsettings.{Environment.MachineName}.json", true, true)
+        .AddEnvironmentVariables()
+        .Build();
+
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(Configuration).WriteTo.Console().CreateLogger();
+
+            CreateHostBuilder(args).Build().Run();
+        }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureCmsDefaults()
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
     }
 }
